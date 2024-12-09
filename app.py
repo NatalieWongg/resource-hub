32ee73ab989671d84ec37317c08ae52f58bbcb23
NatalieWongg <nataliewongxiangpin@gmail.com>
2024-12-09 23:16:22 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Dummy data for now
+resources = []
+
+@app.route('/')
+def home():
+    return render_template('index.html', resources=resources)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        link = request.form['link']
+        resources.append({'title': title, 'description': description, 'link': link})
+        return render_template('index.html', resources=resources)
+    return render_template('add.html')
+
+if __name__ == '__main__':
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
